8aad2e620dac567eb4ec5eab87164a6f4bdc455c
m-szlachta <mszlachta00@gmail.com>
2026-03-25 13:56:19 +0100

    com before downloading model from mlflow

--- src/tests/michal_fake_env/main.py
+++ src/tests/michal_fake_env/main.py
@@ -12,38 +12,48 @@ logging.getLogger("mlflow").setLevel(logging.ERROR)
 accuracy = [0.1, 0.4, 0.35, 0.6, 0.8, 0.90, 0.85]
 loss = [0.9, 0.8, 0.75, 0.6, 0.5, 0.4, 0.35]
 
-model_config_path = "config/config_model_randlanet.json"
-training_config_path = "config/config_train_randlanet.json"
+
 
 def main():
+
+    # TRAINING RUN
+    optuna_model_config_path = "config/config_model_randlanet_0.json"
+    optuna_training_config_path = "config/config_train_randlanet.json"
+
     config = MLFlowConfig.from_json('src/tests/michal_fake_env/mlflow_config.json')
     client = config.apply("Training Run")
 
-    logger = MLFlowTracker(client=client, config=config, number_of_models_to_track=2, min_or_max="max")
-    logger.log_config(config_path=model_config_path, save_config_for_model=False, save_as_parameters=True)
-    logger.log_config(config_path=training_config_path, save_config_for_model=False, save_as_parameters=False)
+    #logging initial configs and dataset
+    logger = MLFlowTracker(client=client, config=config, min_or_max="max")
+    logger.log_config(config_path=optuna_model_config_path, save_config_for_model=False, save_as_parameters=True)
+    logger.log_config(config_path=optuna_training_config_path, save_config_for_model=False, save_as_parameters=False)
     logger.log_dataset(path="config/wynik 1.las")
 
     for idx, i in enumerate(accuracy):
+        model_config_path = "config/config_model_randlanet_1.json"
+        training_config_path = "config/config_train_single_randlanet.json"
         model = Model(model=nn.Sequential(nn.Linear(2,1)),
-                      metrics={"accuracy": i, "loss": loss[idx]}, 
-                      metrics_art={"cm": [[50, 2], [1, 47]]}, 
-                      config="config/config_train_single_randlanet.json",
+                      metrics={"accuracy": i, "loss": loss[idx]},
+                      metrics_art={"cm": [[50, 2], [1, 47]]},
+                      configs=[model_config_path, training_config_path],
                       best_val=i)
         print(model) 
-        logger.log_training(model=model, model_name="Resnet", step=idx)
+        logger.log_training(model=model, model_name="Resnet", number_of_models_to_track=2, step=idx)
 
     config.end_run()
+
+    # EVALUATION RUN
+
+    eval_model_config_path = "config/config_model_randlanet_1.json"
     client = config.apply("Evaluation Run")
-    logger = MLFlowTracker(client=client, config=config, number_of_models_to_track=1, min_or_max=None)
-    logger.log_config(config_path=model_config_path, save_config_for_model=False, save_as_parameters=True)
+    logger = MLFlowTracker(client=client, config=config, min_or_max=None)
     logger.log_dataset(path="config/wynik 1.las")
 
     test_metrics = {"accuracy": 0.88, "loss": 0.38}
     model = Model(model=nn.Sequential(nn.Linear(2,1)),
-                    metrics=test_metrics, 
-                    metrics_art={"cm": [[50, 2], [1, 47]]}, 
-                    config="config/config_train_single_randlanet.json",
+                    metrics=test_metrics,
+                    metrics_art={"cm": [[50, 2], [1, 47]]},
+                    configs=[eval_model_config_path],
                     best_val=test_metrics["accuracy"])
     logger.log_evaluation(model=model, model_name="Resnet_Eval")
 
@@ -52,7 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-        
-
-#accuracy takie same dla każdego modelu
